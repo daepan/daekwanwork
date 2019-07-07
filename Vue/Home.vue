db12740f682f2dd94ada1673d35829130a5bc83b
daepan <50792467+daepan@users.noreply.github.com>
2019-07-08 03:53:06 +0900

    Add files via upload

---
+++ Vue/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  computed: {
+    username () {
+      // We will see what `params` is shortly
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack () {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
