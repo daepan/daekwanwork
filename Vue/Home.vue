9cce20c98b4e6f027e1350c98cc5aeed561581bb
murpan <qw04011@gmail.com>
2019-07-14 00:29:41 +0900

    Merge branch 'master' of https://github.com/daepan/daekwanwork
    # Conflicts:
    #	Vue/Home.vue
    #	Vue/component1.html
    #	Vue/component2.html
    #	Vue/directive.html
    #	Vue/form.html
    #	Vue/list.html
    #	Vue/mixin.html
    #	Vue/render.html
    #	Vue/router.html
    #	Vue/tradistion.html
    #	Vue/vuex.html

--- Vue/Home.vue
+++ Vue/Home.vue
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <template>
   
 </template>
@@ -30,3 +31,37 @@ export default {
   margin-top: 60px;
 }
 </style>
+=======
+<template>
+  
+</template>
+
+<script>
+export default {
+  computed: {
+    username () {
+      // We will see what `params` is shortly
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack () {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+>>>>>>> db12740f682f2dd94ada1673d35829130a5bc83b
